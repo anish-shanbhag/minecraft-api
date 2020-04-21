601de0961097314022e73afac51b1fbc0bddc048
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2020-04-21 14:12:19 -0700

    Add crafting-recipes endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const knex = require("knex")({
     user: "root",
     password: config.dbPassword,
     database: "minecraft",
-    typeCast (field, next) {
+    typeCast(field, next) {
       if (field.type == "TINY" && field.length == 1) {
         return (field.string() == "1");
       } else if (field.type === "JSON") {
@@ -25,8 +25,6 @@ const knex = require("knex")({
 const app = express();
 app.use(cors());
 
-const getParams = (req, params) => params.map(([param, defaultValue]) => req.query[param] ? req.query[param] : defaultValue);
-
 const arrayEndpoint = (tableName, req) => {
   if (req.query.page && !req.query.limit) {
     throw createError(400, "If you include the page parameter, you must also include the limit parameter.");
@@ -36,33 +34,33 @@ const arrayEndpoint = (tableName, req) => {
     ["page", 1],
     ["sort", tableName.slice(0, -1) + "Id"],
     ["order", "asc"],
-    ["include", "*"]
+    ["fields", "*"]
   ];
-  const [limit, page, sort, order, include] = params.map(([param, defaultValue]) => req.query[param] ? req.query[param] : defaultValue);
-  return knex.select(include).from(tableName).orderBy(sort, order).limit(limit).offset(limit * (page - 1));
+  const [limit, page, sort, order, fields] = params.map(([param, defaultValue]) => req.query[param] ? req.query[param] : defaultValue);
+  return knex.select(fields).from(tableName).orderBy(sort, order).limit(limit).offset(limit * (page - 1));
 }
 
 app.get("/items", async (req, res) => res.send(await arrayEndpoint("items", req)));
 
-app.get("/items/:item", async (req, res) => {
-  const item = req.params.item;
+const getItem = async (item, fields = "*") => {
   const column = isNaN(item) ? item.toLowerCase() === item ? "namespacedId" : "name" : "itemId";
-  const include = req.query.include ? req.query.include : "*";
-  const itemObject = await knex.select(include).from("items").where(column, item).first();
+  const itemObject = await knex.select(fields).from("items").where(column, item).first();
   if (itemObject) {
-    res.send(itemObject);
+    return itemObject;
   } else {
     throw createError(404, "The requested item was not found.");
   }
-});
+}
+
+app.get("/items/:item", async (req, res) => res.send(await getItem(req.params.item, req.query.fields)));
 
 app.get("/blocks", async (req, res) => res.send(await arrayEndpoint("blocks", req)));
 
 app.get("/blocks/:block", async (req, res) => {
   const block = req.params.block;
   const column = isNaN(block) ? block.toLowerCase() === block ? "namespacedId" : "name" : "blockId";
-  const include = req.query.include ? req.query.include : "*";
-  const blockObject = await knex.select(include).from("blocks").where(column, block).first();
+  const fields = req.query.fields ? req.query.fields : "*";
+  const blockObject = await knex.select(fields).from("blocks").where(column, block).first();
   if (blockObject) {
     res.send(blockObject);
   } else {
@@ -70,26 +68,69 @@ app.get("/blocks/:block", async (req, res) => {
   }
 });
 
-app.get("/crafting-recipes", async (req, res) => res.send(await arrayEndpoint("craftingRecipes", req)));
+app.get("/crafting-recipes", async (req, res) => {
+  let query = arrayEndpoint("craftingRecipes", req);
+  if (req.query.item) {
+    query.where("itemId", (await getItem(req.query.item, "itemId")).itemId);
+  }
+  if (req.query.uses) {
+    query.whereRaw("recipe REGEXP '^:itemId$|\\\\[:itemId,.+|, :itemId,|, :itemId\\\\]'", {
+      ...await getItem(req.query.uses, "itemId")
+    });
+  }
+  let recipes = await query;
+  if (req.query.itemFields) {
+    const getFlattenedItem = async itemId => {
+      let item = await getItem(itemId, req.query.itemFields);
+      if (Object.values(item).length === 1) return Object.values(item)[0];
+      return item;
+    }
+    recipes = await Promise.all(recipes.map(async ({
+      craftingRecipeId,
+      itemId,
+      quantity,
+      shapeless,
+      recipe
+    }) => {
+      const recipeObject = {
+        craftingRecipeId,
+        quantity,
+        shapeless
+      }
+      if (itemId) {
+        recipeObject.item = await getFlattenedItem(itemId);
+      }
+      if (recipe) {
+        recipeObject.recipe = await Promise.all(recipe.map(async variant => {
+          if (variant) {
+            if (Array.isArray(variant)) {
+              return Promise.all(variant.map(async variantItem => {
+                return variantItem ? getFlattenedItem(variantItem) : null
+              }));
+            } else {
+              return getFlattenedItem(variant);
+            }
+          } else return null;
+        }));
+      }
+      return recipeObject;
+    }))
+  }
+  res.send(recipes);
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, async () => {
   try {
-    const recipes = (await axios.get("http://localhost:4000/crafting-recipes", {
+    const data = (await axios.get("http://localhost:4000/crafting-recipes", {
       params: {
-        page: 2,
-        limit: 20,
+        item: "Firework Star",
+        fields: ["itemId", "recipe"],
+        itemFields: ["name"]
       }
     })).data;
-    console.log(recipes);
+    console.log(data[0]);
   } catch (e) {
     console.error("Client error: " + e);
   }
-  return
-  console.log(await Promise.all(recipes.map(async ({itemId, quantity, shapeless, recipe}) => ({
-    item: (await axios("http://localhost:4000/items/" + itemId)).data,
-    quantity,
-    shapeless,
-    recipe: await Promise.all(recipe.map(async craftingVariant => craftingVariant ? (await axios("http://localhost:4000/items/" + craftingVariant)).data.name : null))
-  }))));
 });
